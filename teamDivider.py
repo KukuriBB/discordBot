5520af48cb4d8651db7a84e06c7f56af92e4b0f0
daiki mitsumoto <d.mitsumoto@gmail.com>
2018-07-27 03:15:03 +0900

    add random.seed()

--- teamDivider.py
+++ teamDivider.py
@@ -13,7 +13,7 @@ def countMembers(content):
             
     
     if(channelID==""):
-        return "USAGE: !count <channel ID>"
+        return "USAGE: count <channel ID>"
     
     # get channel info
     channel=bot.get_channel(channelID)
@@ -46,16 +46,18 @@ def startDivide(content):
             
     
     if(channelID==""):
-        return "USAGE: !roll <channel ID> [options]"
+        return "USAGE: roll <channel ID> [options]"
     
     # get channel info
     channel=bot.get_channel(channelID)
     if channel==None:
         return "Error: %s doesn't exist" % arg
     
+    # check channel type
     elif str(channel.type)=="text":
         return "Error: '" + channel.name + "' is text channel"
         
+    
     elif not channel.voice_members:
         return "Error: no one is in '" + channel.name + "'"
         
@@ -63,6 +65,7 @@ def startDivide(content):
         print(channel.name)
         print("%d men a team" % div)
         
+        random.seed()
         randomList=random.sample(channel.voice_members, len(channel.voice_members))
         m=""
         for i in range(len(randomList)):
@@ -72,17 +75,17 @@ def startDivide(content):
 
 
 def testCommands():
-    print( startDivide("!roll") )
-    print( startDivide("!roll 47166010791952384") )
-    print( startDivide("!roll 471660107919523843") )
-    print( startDivide("!roll 471660107919523845 -n") )
-    print( startDivide("!roll 471660107919523845") )
-    print( startDivide("!roll -n3 471660107919523845") )
-    print( startDivide("!roll 471660107919523845 -n4") )
-    print( countMembers("!count") )
-    print( countMembers("!count 47166010791952384") )
-    print( countMembers("!count 471660107919523843") )
-    print( countMembers("!count 471660107919523845") )
+    print( startDivide("roll") )
+    print( startDivide("roll 47166010791952384") )
+    print( startDivide("roll 471660107919523843") )
+    print( startDivide("roll 471660107919523845 -n") )
+    print( startDivide("roll 471660107919523845") )
+    print( startDivide("roll -n3 471660107919523845") )
+    print( startDivide("roll 471660107919523845 -n4") )
+    print( countMembers("count") )
+    print( countMembers("count 47166010791952384") )
+    print( countMembers("count 471660107919523843") )
+    print( countMembers("count 471660107919523845") )
 
 
 @bot.event
@@ -99,11 +102,11 @@ async def on_message(message):
     # ignore itself
     if bot.user != message.author:   
         # check command
-        if message.content.startswith("!roll"):
+        if message.content.startswith("roll"):
             print('===divide===')
             m=startDivide(message.content)
             await bot.send_message(message.channel, m)
-        elif message.content.startswith("!count"):
+        elif message.content.startswith("count"):
             print('===count===')
             m=countMembers(message.content)
             await bot.send_message(message.channel, m)
