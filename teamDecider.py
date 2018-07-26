92b1cbfb54adc4747878767dcf51d89b6cea22d6
daiki mitsumoto <d.mitsumoto@gmail.com>
2018-07-26 17:03:31 +0900

    add count function

--- teamDecider.py
+++ teamDecider.py
@@ -1,56 +1,112 @@
 import discord
 import random
 
-client = discord.Client()
+bot = discord.Client()
 
-def startDevide(content, div=2):
-    m=""
-    args=content.split(" ")
+def countMembers(content):
+    channelID=""
+    
+    # read argments
+    for arg in content.split(" ")[1:]:
+        # get channel
+        channelID=arg
+            
+    
+    if(channelID==""):
+        return "USAGE: !count <channel ID>"
+    
+    # get channel info
+    channel=bot.get_channel(channelID)
+    if channel==None:
+        return "Error: %s doesn't exist" % arg
+    
+    elif str(channel.type)=="text":
+        return "Error: '" + channel.name + "' is text channel"
+        
+    return "%d" % len(channel.voice_members)
 
-    if(len(args)==1):
-        m+="USAGE: !roll <channel ID>"
+
+def startDevide(content):
+    channelID=""
+    div=2
+    
+    # read argments
+    for arg in content.split(" ")[1:]:
+        # divitions option
+        if arg.startswith("-n"):
+            # check extra option
+            if len(arg)==2:
+                return "Error: -n need number (ex: -n4)"
+                
+            div=int(arg[2:])
+        else:
+            # get channel
+            channelID=arg
+            #channelID=bot.get_channel(arg)
+            
+    
+    if(channelID==""):
+        return "USAGE: !roll <channel ID> [options]"
+    
+    # get channel info
+    channel=bot.get_channel(channelID)
+    if channel==None:
+        return "Error: %s doesn't exist" % arg
+    
+    elif str(channel.type)=="text":
+        return "Error: '" + channel.name + "' is text channel"
+        
+    elif not channel.voice_members:
+        return "Error: no one is in '" + channel.name + "'"
         
     else:
-        channel=client.get_channel(args[1])
-        if channel==None:
-            m+="Error: specified channel doesn't exist (" + args[1] + ")"
-        elif str(channel.type)=="text":
-            m+="Error: '" + channel.name + "' is text channel"
-        elif not channel.voice_members:
-            m+="Error: no member in '" + channel.name + "'"
-        else:
-            m+=channel.name+"\n"
-            m+=str(channel.type)+"\n"
-            randomList=random.sample(channel.voice_members, len(channel.voice_members))
-            for i in range(len(randomList)):
-                m+="#" + str(int( (i+div)/div )) + " " + randomList[i].name+"\n"
+        print(channel.name)
+        print("%d men in team" % div)
         
-    m+="\n"
+        randomList=random.sample(channel.voice_members, len(channel.voice_members))
+        m=""
+        for i in range(len(randomList)):
+            m+="#" + str(int( (i+div)/div )) + " " + randomList[i].name+"\n"
     
     return m
 
-@client.event
+
+def testCommands():
+    print( startDevide("!roll") )
+    print( startDevide("!roll 47166010791952384") )
+    print( startDevide("!roll 471660107919523843") )
+    print( startDevide("!roll 471660107919523845 -n") )
+    print( startDevide("!roll 471660107919523845") )
+    print( startDevide("!roll -n3 471660107919523845") )
+    print( startDevide("!roll 471660107919523845 -n4") )
+    print( countMembers("!count") )
+    print( countMembers("!count 47166010791952384") )
+    print( countMembers("!count 471660107919523843") )
+    print( countMembers("!count 471660107919523845") )
+
+
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    #for member in client.get_all_members():
-    print(startDevide("!roll"))
-    print(startDevide("!roll 47166010791952384"))
-    print(startDevide("!roll 471660107919523843"))
-    print(startDevide("!roll 471660107919523845"))
-    
-    print('------')
+    print(bot.user.name)
+    print(bot.user.id)
+    #for member in bot.get_all_members():
+    #testCommands()
+    print('===ready===')
 
-@client.event
+@bot.event
 async def on_message(message):
-    # 送り主がBotだった場合反応したくないので
-    if client.user != message.author:
-        # 「おはよう」で始まるか調べる
+    # ignore itself
+    if bot.user != message.author:   
+        # check command
         if message.content.startswith("!roll"):
+            print('===devide===')
             m=startDevide(message.content)
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await client.send_message(message.channel, m)
+            await bot.send_message(message.channel, m)
+        elif message.content.startswith("!count"):
+            print('===count===')
+            m=countMembers(message.content)
+            await bot.send_message(message.channel, m)
 
 # token にDiscordのデベロッパサイトで取得したトークンを入れてください
-client.run("NDcxNjU5MjgzNTU4MTcwNjI0.DjoTgQ.xZF_8mR26TPoCa1RFVwtkYH8Bvg")
+bot.run("NDcxNjU5MjgzNTU4MTcwNjI0.DjoTgQ.xZF_8mR26TPoCa1RFVwtkYH8Bvg")
